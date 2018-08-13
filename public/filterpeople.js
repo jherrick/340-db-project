10d16288ebea55ed79799dfc362f752fd8a9f354
djbauman <danieljbauman@gmail.com>
2018-08-13 14:01:52 -0700

    attempt to add filter

--- public/filterpeople.js
+++ public/filterpeople.js
@@ -1,7 +1,7 @@
-function filterCharacters() {
-    console.log("Hello from filterCharacters()!");
+function filterPeopleBySchool() {
+    console.log("Hello from filterPeopleBySchool()!");
     //get the id of the selected house from the filter dropdown
     var school_id = document.getElementById('school_filter').value
     //construct the URL and redirect to it
     window.location = '/characters/filter/' + parseInt(school_id)
-}
+};
